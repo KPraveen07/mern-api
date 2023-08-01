3c554dafa692683241f64b04feaa93a01ab5612b
Kpraveen07 <Kpraveenk701@gmail.com>
2023-08-01 14:02:02 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 
 const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
+    //res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
