08326a66ec5fc7db9b93a1db28bd116719aa482b
Kpraveen07 <Kpraveenk701@gmail.com>
2023-08-03 00:25:22 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -11,32 +11,7 @@ app.use(cors(
         credentials : true
     }
 ))
-
-// const allowCors = fn => async (req, res) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', 'https://mern-frontend-steel.vercel.app')
-//     // another common pattern
-//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return await fn(req, res)
-//   }
   
-//   const handler = (req, res) => {
-//     const d = new Date()
-//     res.end(d.toString())
-//   }
-  
-//   module.exports = allowCors(handler)
-  
-
 app.use(express.json())
 
 //mongoose.connect("mongodb://127.0.0.1:27017/MERN")
@@ -93,9 +68,6 @@ app.delete('/deleteItem/:id', (req, res) => {
     .then(res => res.json(res))
     .catch(err => json(err))
 })
-// app.get('/cors', (req, res) => {
-//     res.send('This has CORS enabled 🎈')
-// })
 
 app.get('*',(req,res)=>{
     res.status(200).json({
